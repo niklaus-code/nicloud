03bd7b6f068118cdbfe5dad83d81fe09eaf34049
ysman <1309584951@qq.com>
2021-10-29 16:30:16 +0800

    ok

---
+++ src/view/ips.vue
@@ -0,0 +1,125 @@
+<template>
+	<div>
+	<nicloudhead></nicloudhead>
+	<vmleft></vmleft>
+  	<div class="content whisper-content leacots-content details-content col-md-11 col-md-offset-2" style="background-color:white; float:left">
+		<div class="col-sm-10 col-sm-offset-1 vlan" style="margin-top:20px;">
+			<span>{{vlan}}&nbsp&&nbsp IP列表</span>
+		</div>
+		<div class="col-sm-10 col-sm-offset-1" style="margin-top:20px;">
+			<table class="table table-hover" style="text-align: center;">
+    			<thead>
+      				<tr>
+        				<th>
+        					<label class="checkbox-inline" style="border:red 1px">
+            					<input type="checkbox" v-model="checkvalue" @click="checkbox()">
+        					</label>
+        				</th>
+						<th>IPv4</th>
+						<th>MACADDR</th>
+						<th>状态</th>
+						<th>操作</th>
+      				</tr>
+    			</thead>
+
+				<tbody v-for="(item, index) in ips">
+      				<tr class="table-dark text-dark" :id="item.Ipv4">
+        				<label class="checkbox-inline">
+            				<input type="checkbox" v-model="item.Checkout">
+        				</label>
+        				<td>{{item.Ipv4}}</td>
+        				<td>{{item.Macaddr}}</td>
+        				<td>
+							<span v-if="item.Status"  class="glyphicon glyphicon-remove"></span>
+							<span else  class="glyphicon glyphicon-ok"></span>
+						</td>
+		    			<td>
+							<button class="btn btn-danger btn-xs" type="button" @click="">
+                				删除
+            				</button>
+        				</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-sm-10 col-sm-offset-1" style="margin-top:20px;">
+		</div>
+	</div>		
+</div>		
+</template>
+<script>
+import foot from '@/components/footer'
+import nicloudhead from '@/components/nicloudhead'
+import vmleft from '@/components/vmleft'
+
+
+export default {
+    data () {
+        return {
+			vlan: {},
+			ips: [],
+        }
+    },
+
+    components: {
+        foot, nicloudhead, vmleft
+    },
+
+
+	  created: function () {
+        this.vlaninfo()
+    },
+
+	mounted: function () {
+		this.getip()
+		},
+
+    methods: {
+	   vlaninfo: function () {
+            this.vlan = this.$route.query.vlan
+            },
+
+		 getip: function () {
+            var apiurl = `/api/networks/getip`
+            this.$http.get(apiurl, { params: { vlan: this.vlan}}).then(response => {
+            	this.ips = response.data.res
+            })
+        },
+  }
+  }
+</script>
+<style>
+.vlan {
+	font-size: 18px;
+	font-weight: 600;
+	border-bottom: 3px solid green
+}
+
+.content {
+    box-shadow: 0 0 10px rgba(0,0,0,8);
+    border-radius: 10px/10px;
+    z-index: -1;
+    padding: 50px 0px 50px 0px;
+    margin-left: 0px;
+    margin-TOP: 50px;
+}
+.checkbox-inline {
+    margin-bottom: 30px;
+}
+
+.details-content .article-cont p {
+    padding:30px 0 0 5px
+}
+
+.table tbody tr td {
+    padding: 12px;
+    vertical-align: "middle";
+}
+
+th {
+	font-weight: bold;
+    color: black;
+    text-align: center;
+}
+
+</style>
