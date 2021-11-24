162e8026bd868d8d60c20a9963e330c1ff6df9f1
ysman <1309584951@qq.com>
2021-11-24 14:33:15 +0800

    login css image

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,24 +1,28 @@
 <template>
-<div class="login col-sm-12">
-	<div class="col-sm-2 col-sm-offset-5" style="margin-top:150px">
-		<form class="form-horizontal">
-  		<div class="form-group">
-    		<div class="col-sm-10">
-      			<input v-model="username" class="form-control" placeholder="账号">
-    		</div>
-  		</div>
-  		<div class="form-group">
-    		<div class="col-sm-10">
-      			<input v-model="passwd" type="password" class="form-control" placeholder="密码">
-    		</div>
-  		</div>
-  		<div class="form-group">
-    		<div class="col-sm-10">
-      			<input type="button" @click="login" value="登陆">
-    		</div>
-  		</div>
+<div class="login col-sm-12" :style="login">
+	<div class="col-sm-4 col-sm-offset-4" style="margin-top:250px; display: flex; 　align-items:center;">
+		<form class="form-horizontal col-sm-12">
+			<div class="col-sm-10 col-sm-offset-1 a">
+			<div class="col-sm-8 col-sm-offset-2 ">
+  				<div class="form-group">
+    				<div class="col-sm-12">
+      					<input v-model="username" class="form-control passwd" :style="userimage" placeholder="账号">
+    				</div>
+  				</div>
+  				<div class="form-group">
+    				<div class="col-sm-12">
+      					<input v-model="passwd" type="password" :style="passimage" class="form-control passwd" placeholder="密码">
+    				</div>
+  				</div>
+  				<div class="form-group">
+    				<div class="col-sm-12">
+      					<input type="button" @click="login" value="登陆">
+    				</div>
+  				</div>
+  			</div>
+  			</div>
 		</form>
-  </div>
+  	</div>
 </div>
 </template>
 <script>
@@ -27,7 +31,16 @@ export default {
         return {
             username: "",
             passwd: "",
-        }
+			login: {
+			backgroundImage:"url(" + require("./fish.jpg") + ")",
+        	},
+			passimage: {
+				backgroundImage:"url(" + require("./pass.svg") + ")",
+        	},
+			userimage: {
+				backgroundImage:"url(" + require("./user.svg") + ")",
+        	}
+		}	
     },
     methods: {
 		login: function (index, uuid) {
@@ -49,16 +62,37 @@ export default {
 
 </script>
 <style scoped>
+.passwd {
+	padding: 0 10px 0 35px;
+	background-position: 10px 8px;
+	background-repeat: no-repeat;
+}
+
 .login {
 	position:fixed;
 	height:100%;
-	background-color:#ffffff;
+	#background-color:#ffffff;
 	width:100%
 	}
+
+.a {
+	background-color:rgba(255,255,255,0.30);
+}
+
+.glyphicon {
+	display: inline-block;
+	 background-repeat: no-repeat;
+	 top: 24%;
+	z-index: 2;
+}
 
 input {
 	width: 100%;
 	font-family: "微软雅黑";
 }
 
+.col-sm-8 {
+	margin-top:30px;
+	margin-bottom:10px;
+}
 </style>
