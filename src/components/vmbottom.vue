3e666e298705eb9f0b9a70ec5412cb5d6052f125
Your Name <you@example.com>
2021-12-21 14:12:39 +0800

    bottom

---
+++ src/components/vmbottom.vue
@@ -0,0 +1,16 @@
+<template>
+    <div align="center" class="col-md-12">
+        <h5>中科院网络信息中心 & 大数据部</h5>
+    </div>
+</template>
+
+<style>
+.col-md-12 {
+    bottom:0; 
+    position: fixed;
+    background-color: #FFFFFF;
+    font-weight:500;
+    padding-top: 4px;
+    padding-bottom: 4px;
+}
+</style>
