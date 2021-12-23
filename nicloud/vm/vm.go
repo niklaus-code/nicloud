aea0ed65c5c0b609bac8a36c55ef1580d9aa5785
Your Name <you@example.com>
2021-12-23 17:48:40 +0800

    pagenum fix

--- nicloud/vm/vm.go
+++ nicloud/vm/vm.go
@@ -518,7 +518,10 @@ func Getpagenumber(user string, offset int) (int, error) {
   } else {
     dbs.Table("vms").Order("create_time desc").Where("owner=?", user).Order("create_time desc").Select([]string{"uuid", "name", "cpu", "mem", "owner", "comment", "status", "storage", "datacenter", "exist", "ip", "host", "os"}).Scan(&v)
   }
-  pagenumber := len(v)/offset+1
+  pagenumber := len(v)%offset
+  if pagenumber > 0 {
+    pagenumber = pagenumber + 1
+  }
   return pagenumber, nil
 }
 
