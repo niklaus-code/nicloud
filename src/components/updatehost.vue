29e758b786cee8c5071e12e3c12c623f28e4772e
Your Name <you@example.com>
2022-03-21 17:13:26 +0800

    updatehost

---
+++ src/components/updatehost.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <updatehosthead></updatehosthead>
+    <updatehostform></updatehostform>
+</div>
+</template>
+<script>
+import updatehosthead from './updatehosthead'
+import updatehostform from './updatehostform'
+export default {
+    data () {
+        return {
+        }
+    },
+
+    components: {
+        updatehosthead, updatehostform
+        },
+  }
+</script>
+
+<style scoped>
+</style>
