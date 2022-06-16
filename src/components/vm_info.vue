f1af00dfb3bc6dffeeb3f88741dbb61407aa12c1
Your Name <you@example.com>
2022-06-16 10:28:14 +0800

    修改 监控 前端请求时间为2s  区间为 60s

--- src/components/vm_info.vue
+++ src/components/vm_info.vue
@@ -194,22 +194,22 @@ export default {
     this.getcpuChart();
     this.cputimer = setInterval(() => {
         this.getcpuChart();
-        }, 1000);
+        }, 2000);
 
     this.getmemChart();
     this.memtimer = setInterval(() => {
         this.getmemChart();
-        }, 1000);
+        }, 2000);
 
     this.getChart();
     this.nettimer = setInterval(() => {
         this.getChart();
-        }, 1000);
+        }, 2000);
 
     this.getdiskChart();
     this.disktimer = setInterval(() => {
         this.getdiskChart();
-        }, 1000);
+        }, 2000);
   },
 
     beforeDestroy() {
@@ -245,12 +245,12 @@ export default {
                  alert(response.data.err.Message)
              } else {
                  this.cpuchart.xAxis.categories.push(response.data.res["Ctime"])
-                 if (this.cpuchart.xAxis.categories.length > 20 ) {
+                 if (this.cpuchart.xAxis.categories.length > 60 ) {
                     this.cpuchart.xAxis.categories.splice(0, 1)
                     }
 
                  this.cpuchart.series[0].data.push(response.data.res["Load"])
-                 if (this.cpuchart.series[0].data.length > 20 ) {
+                 if (this.cpuchart.series[0].data.length > 60 ) {
                     this.cpuchart.series[0].data.splice(0, 1)
                     }
                 }
@@ -264,22 +264,22 @@ export default {
                  alert(response.data.err.Message)
              } else {
                  this.memchart.xAxis.categories.push(response.data.res["Ctime"])
-                 if (this.memchart.xAxis.categories.length > 20 ) {
+                 if (this.memchart.xAxis.categories.length > 60 ) {
                     this.memchart.xAxis.categories.splice(0, 1)
                     }
 
                  this.memchart.series[0].data.push(response.data.res["Mem_total"])
-                 if (this.memchart.series[0].data.length > 20 ) {
+                 if (this.memchart.series[0].data.length > 60 ) {
                     this.memchart.series[0].data.splice(0, 1)
                     }
 
                  this.memchart.series[1].data.push(response.data.res["Mem_used"])
-                 if (this.memchart.series[1].data.length > 20 ) {
+                 if (this.memchart.series[1].data.length > 60 ) {
                     this.memchart.series[1].data.splice(0, 1)
                     }
 
                  this.memchart.series[2].data.push(response.data.res["Mem_availabled"])
-                 if (this.memchart.series[2].data.length > 20 ) {
+                 if (this.memchart.series[2].data.length > 60 ) {
                     this.memchart.series[2].data.splice(0, 1)
                     }
                 }
@@ -293,17 +293,17 @@ export default {
                  alert(response.data.err.Message)
              } else {
                  this.diskchart.xAxis.categories.push(response.data.res["Ctime"])
-                 if (this.diskchart.xAxis.categories.length > 20 ) {
+                 if (this.diskchart.xAxis.categories.length > 60 ) {
                     this.diskchart.xAxis.categories.splice(0, 1)
                     }
 
                  this.diskchart.series[0].data.push(response.data.res["Read"])
-                 if (this.diskchart.series[0].data.length > 20 ) {
+                 if (this.diskchart.series[0].data.length > 60 ) {
                     this.diskchart.series[0].data.splice(0, 1)
                     }
 
                  this.diskchart.series[1].data.push(response.data.res["Write"])
-                 if (this.diskchart.series[1].data.length > 20 ) {
+                 if (this.diskchart.series[1].data.length > 60 ) {
                     this.diskchart.series[1].data.splice(0, 1)
                     }
                 }
@@ -317,17 +317,17 @@ export default {
                  alert(response.data.err.Message)
              } else {
                  this.netchart.xAxis.categories.push(response.data.res["Ctime"])
-                 if (this.netchart.xAxis.categories.length > 20 ) {
+                 if (this.netchart.xAxis.categories.length > 60 ) {
                     this.netchart.xAxis.categories.splice(0, 1)
                     }
 
                  this.netchart.series[0].data.push(response.data.res["Rx"])
-                 if (this.netchart.series[0].data.length > 20 ) {
+                 if (this.netchart.series[0].data.length > 60 ) {
                     this.netchart.series[0].data.splice(0, 1)
                     }
 
                  this.netchart.series[1].data.push(response.data.res["Tx"])
-                 if (this.netchart.series[1].data.length > 20 ) {
+                 if (this.netchart.series[1].data.length > 60 ) {
                     this.netchart.series[1].data.splice(0, 1)
                     }
                 }
